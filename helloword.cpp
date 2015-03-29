3fb4c4e277538d4fc1b0c4fa50263ae3f901e2ef
LeandroMaxNatal <leandromaxnatal@gmail.com>
2015-03-29 00:59:03 -0300

    fist commit on github

---
+++ helloword.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char const *argv[])
+{
+	cout << "Hello World!" << endl; // this line print a string Hello world on dysplay
+	system("pause");
+	return 0;
+}
